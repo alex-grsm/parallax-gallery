5538574f6e3a1b640db88d8d4063a6fdac61449c
Alex Grsm <alexey.grsm@gmail.com>
2022-12-12 18:21:24 +0200

    done

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -2,8 +2,8 @@
 // Рекомендуется создавать отдельный файл в папке components и подключать все там
 
 // Определение операционной системы на мобильных
-import { mobileCheck } from "./functions/mobile-check";
-console.log(mobileCheck())
+// import { mobileCheck } from "./functions/mobile-check";
+// console.log(mobileCheck())
 
 // Определение ширины экрана
 // import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
